7515365fcfc5acb6dedfc0624d79ef654cbef9f0
Arthur Jaillard <arthur.jaillard@edu.ece.fr>
2020-10-13 00:47:55 +0200

    version finale

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
-
-// Fonction appelée lors du click du bouton
-function start() {
+  
+function start(){
   // Création de l'objet apiWeather
   const apiWeather = new API_WEATHER();
   // Appel de la fonction fetchTodayForecast
+ var city =  document.getElementsByTagName("input")[0].value;
 
   apiWeather
-    .fetchTodayForecast()
+    .fetch4DaysForecast(city)
     .then(function(response) {
+      console.log(response.data)
       // Récupère la donnée d'une API
       const data = response.data;
+      
+      for(i = 0; i < 4; i++){
+        htmlEl = "today";
 
-      // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
-
-      // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+        if(i == 1) htmlEl = "demain";
+        else if(i == 2) htmlEl = 'ademain';
+        else if(i == 3) htmlEl = "eademain";
+       
+           // On récupère l'information principal
+         const main = data.list[i].weather[0].main;
+         const description = data.list[i].weather[0].description;
+         const temp = data.list[i].temp.day;
+         const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
+   
+         // Modifier le DOM
+         document.getElementById(htmlEl+'-forecast-main').innerHTML = main;
+         document.getElementById(htmlEl+'-forecast-more-info').innerHTML = description;
+         document.getElementById(htmlEl+'-icon-weather-container').innerHTML = icon;
+         document.getElementById(htmlEl+'-forecast-temp').innerHTML = `${temp}°C`;
+      }
       
     })
     .catch(function(error) {
